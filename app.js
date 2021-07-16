07b0916e14372ebf5c031028ea1a679c738a3515
dev-from-cbd <veryfatwombat@gmail.com>
2021-07-16 10:09:05 +1000

    js

---
+++ app.js
@@ -0,0 +1,10 @@
+// classList - shows/gets all classes
+// contains - checks classList for specific class
+// add - add class
+// remove - remove class
+// toggle - toggles class
+
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+//const navCenter = document.querySelector(".nav-center");
+//const navHeader = document.querySelector(".nav-header");
