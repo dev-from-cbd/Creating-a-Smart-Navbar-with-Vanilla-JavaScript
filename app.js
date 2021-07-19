7718b180ddae97eae8fde01aab2b9b14cae6d587
dev-from-cbd <veryfatwombat@gmail.com>
2021-07-20 09:23:48 +1000

    js

--- app.js
+++ app.js
@@ -10,10 +10,14 @@ const links = document.querySelector(".links");
 navToggle.addEventListener("click", function () {
   //console.log(links.classList.contains("random"));
   //console.log(links.classList.contains("links"));
+
+  /*
   if (links.classList.contains("show-links")) {
     links.classList.remove("show-links");
   } else {
     links.classList.add("show-links");
   }
+  */
+
   links.classList.toggle("show-links");
 });
